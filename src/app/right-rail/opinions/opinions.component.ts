d585393cc408594b34d5780f638b9ad0a99e2262
CASician <cristian.sician@gmail.com>
2024-05-16 22:57:08 +0200

    feat: add opinions and posta del cuore

--- src/app/right-rail/opinions/opinions.component.ts
+++ src/app/right-rail/opinions/opinions.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ItemArticleComponent } from '../item-article/item-article.component';
+import { article } from '../article.model';
 
 @Component({
   selector: 'app-opinions',
   standalone: true,
-  imports: [],
+  imports: [ ItemArticleComponent ],
   templateUrl: './opinions.component.html',
   styleUrl: './opinions.component.css'
 })
 export class OpinionsComponent {
+  @Input() opinion_1!: article;
+  @Input() opinion_2!: article;
+  @Input() opinion_3!: article;
+  @Input() opinion_4!: article;
 
+  @Input() posta!: article;
 }
