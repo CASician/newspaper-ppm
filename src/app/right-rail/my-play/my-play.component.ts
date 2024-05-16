b25fae4b78f0437fae632fcc4d8709f504b763b1
CASician <cristian.sician@gmail.com>
2024-05-16 17:32:34 +0200

    feat: add my-play

--- src/app/right-rail/my-play/my-play.component.ts
+++ src/app/right-rail/my-play/my-play.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { article } from '../article.model';
+import { ItemArticleComponent } from '../item-article/item-article.component';
 
 @Component({
   selector: 'app-my-play',
   standalone: true,
-  imports: [],
+  imports: [ ItemArticleComponent ],
   templateUrl: './my-play.component.html',
   styleUrl: './my-play.component.css'
 })
 export class MyPlayComponent {
-
+  @Input() article_1!: article;
+  @Input() article_2!: article;
+  @Input() article_3!: article;
 }
