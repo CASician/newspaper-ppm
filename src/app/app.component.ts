4b86dbf48a51328e241e770cbbe63182f3ef3fbf
CASician <cristian.sician@gmail.com>
2024-05-17 12:29:55 +0200

    structure: restructure the head-container. Now it contains all header related components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderContainerComponent } from './header-container/header-container.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { RightRailComponent } from './right-rail/right-rail.component';
+import { AreaBottomComponent } from './area-bottom/area-bottom.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,9 @@ import { RightRailComponent } from './right-rail/right-rail.component';
     RouterOutlet, 
     HeaderContainerComponent,
     MainContentComponent,
-    RightRailComponent
+    RightRailComponent,
+    AreaBottomComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/area-bottom/area-bottom.component.ts
+++ src/app/area-bottom/area-bottom.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { BigStrip4Component } from './big-strip-4/big-strip-4.component';
+import { BigStrip3Component } from './big-strip-3/big-strip-3.component';
+import { SportBlueComponent } from './sport-blue/sport-blue.component';
 
 @Component({
   selector: 'app-area-bottom',
   standalone: true,
-  imports: [],
+  imports: [
+    BigStrip4Component,
+    BigStrip3Component,
+    SportBlueComponent
+  ],
   templateUrl: './area-bottom.component.html',
   styleUrl: './area-bottom.component.css'
 })
