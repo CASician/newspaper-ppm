2dbf20fb835d1fcdc57f6d7101a5dadac41d65ff
CASician <cristian.sician@gmail.com>
2024-05-17 10:39:14 +0200

    feat: add oroscopo

--- src/app/right-rail/oroscopo/oroscopo.component.ts
+++ src/app/right-rail/oroscopo/oroscopo.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { article } from '../article.model';
 
 @Component({
   selector: 'app-oroscopo',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './oroscopo.component.css'
 })
 export class OroscopoComponent {
-
+  @Input() segno!: article;
 }
