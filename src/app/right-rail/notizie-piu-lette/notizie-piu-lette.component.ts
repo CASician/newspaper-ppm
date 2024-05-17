33e8d1073353e0b3060a0d4a404e67c06f9ce902
CASician <cristian.sician@gmail.com>
2024-05-17 11:56:25 +0200

    feat: add notizie-piu-lette

--- src/app/right-rail/notizie-piu-lette/notizie-piu-lette.component.ts
+++ src/app/right-rail/notizie-piu-lette/notizie-piu-lette.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { article } from '../article.model';
 
 @Component({
   selector: 'app-notizie-piu-lette',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './notizie-piu-lette.component.css'
 })
 export class NotiziePiuLetteComponent {
-
+  @Input() article_1!: article;
+  @Input() article_2!: article;
+  @Input() article_3!: article;
+  @Input() article_4!: article;
+  @Input() article_5!: article;
 }
