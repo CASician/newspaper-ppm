30d3bbda4cb7ddb3e96f1fae66e9e4f73ea4caae
CASician <cristian.sician@gmail.com>
2024-05-18 20:18:03 +0200

    feat: trying to add a carousel- first attempt before updating angular

--- src/app/main-content/accadde-oggi/accadde-oggi.component.ts
+++ src/app/main-content/accadde-oggi/accadde-oggi.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../article.model';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-accadde-oggi',
   standalone: true,
-  imports: [],
+  imports: [
+    
+  ],
   templateUrl: './accadde-oggi.component.html',
   styleUrl: './accadde-oggi.component.css'
 })
 export class AccaddeOggiComponent {
-
+  @Input() article_1!: Article;
+  @Input() article_2!: Article;
+  @Input() article_3!: Article;
 }
