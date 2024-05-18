ccd04dd42ed88476f107fc084f622e82916b0900
CASician <cristian.sician@gmail.com>
2024-05-18 18:49:42 +0200

    feat: add video del giorno

--- src/app/main-content/video-del-giorno/video-del-giorno.component.ts
+++ src/app/main-content/video-del-giorno/video-del-giorno.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video-del-giorno',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './video-del-giorno.component.css'
 })
 export class VideoDelGiornoComponent {
-
+  @Input() videoUrl!: string;
+  @Input() description!: string;
 }
