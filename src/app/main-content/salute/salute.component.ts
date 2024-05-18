9537941ec6afe36174e8321306b845e8d9b2fb69
CASician <cristian.sician@gmail.com>
2024-05-18 19:13:18 +0200

    feat: add salute

--- src/app/main-content/salute/salute.component.ts
+++ src/app/main-content/salute/salute.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../article.model';
+import { Strip2Component } from '../strips/strip-2/strip-2.component';
 
 @Component({
   selector: 'app-salute',
   standalone: true,
-  imports: [],
+  imports: [ Strip2Component ],
   templateUrl: './salute.component.html',
   styleUrl: './salute.component.css'
 })
 export class SaluteComponent {
-
+  @Input() article_1!: Article;
+  @Input() article_2!: Article;
+  @Input() article_3!: Article;
 }
